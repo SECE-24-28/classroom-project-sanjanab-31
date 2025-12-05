576ec468e34001ad8adc8feb4cbff264286bec68
Sanju <sanjana.b0831@gmail.com>
2025-12-05 09:17:13 +0530

    Objects 1

---
+++ DAY-4/ObjectExa_1.js
@@ -0,0 +1,10 @@
+let student=[{sna:"Praveen", age:19, sma:90, fee:true},
+            {sna:"Ajith", age:18, sma:98, fee:true},
+            {sna:"Kumar", age:20, sma:85, fee:false},
+]
+
+let res= student.filter((stu)=>
+{
+    return stu.sma>=35
+})
+console.log(res)
