505cd7d1d757cc9c9ab1ef36e687aa59759f1ede
Sanju <sanjana.b0831@gmail.com>
2025-12-07 19:04:23 +0530

    Arrow

---
+++ DAY-5/Arrow.js
@@ -0,0 +1,16 @@
+function demo()
+{
+    console.log("Demo Function")
+}
+demo()
+//---------------------------
+let samp=()=>
+{
+    console.log("Arrow Function");
+    return "India";
+}
+samp();
+//---------------------------
+let press=(a,b)=>a+b;
+
+console.log(press(11,22))
